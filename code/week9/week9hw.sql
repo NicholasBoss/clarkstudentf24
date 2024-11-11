f94d52366506a72e3f890b8fa7ef3dc7805ab5d0
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-11-11 10:28:07 -0700

    unlocked week 9 files

---
+++ code/week9/week9hw.sql
@@ -0,0 +1,54 @@
+-- -------------------------------------------------------------------------------------------
+
+USE v_art;
+
+
+-- -------------------------------------------------------------------------------------------
+-- 1. When you visit art.byuiwebdev.com, and you search by artist last name 
+--    and you choose Picasso, you get one resulting image ("guitarist"). 
+--    Write the query that will display this image.
+-- -------------------------------------------------------------------------------------------
+
+
+-- -------------------------------------------------------------------------------------------
+-- 2. When you visit art.byuiwebdev.com, and you search by Subject and type in the word land, 
+--    you get four images. Write the query to display those images.
+--   (remember the keyword might have been 'landscape' but they typed 'land')
+-- -------------------------------------------------------------------------------------------
+
+
+-- -------------------------------------------------------------------------------------------
+-- 3. List the artist without a piece of artwork.
+--    We need the first and last name and artwork title. 
+-- -------------------------------------------------------------------------------------------
+
+
+
+USE magazine;
+
+-- -------------------------------------------------------------------------------------------
+-- 4. List all magazines without a subscriber
+--    List magazine name, and the subscriber's first and last name.
+-- -------------------------------------------------------------------------------------------
+
+
+-- -------------------------------------------------------------------------------------------
+-- 5.  List all the magazines that Albert Anderson subscribes to. 
+-- -------------------------------------------------------------------------------------------
+
+
+
+USE employees;
+
+
+-- -------------------------------------------------------------------------------------------
+-- 6. List the first 5 employees from the Marketing Department. 
+--    Put them in alphabetical order by last name.
+-- -------------------------------------------------------------------------------------------
+
+
+-- -------------------------------------------------------------------------------------------
+-- 7. Find out the current salary and department of Tuval Kalloufi. 
+--    You can use the ORDER BY and LIMIT to get just the most recent salary. 
+--    Format the salary to be readable (Use FORMAT())
+-- -------------------------------------------------------------------------------------------
